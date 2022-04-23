3208f8021e14e7549d0d2f2f3d5497de154e1b60
gomesluiz <gomesluiz@gmail.com>
2022-04-23 20:59:07 -0300

    setup YouCompleMe vim plugin for Fedora 35

---
+++ ItemPedido.java
@@ -0,0 +1,12 @@
+
+public class ItemPedido
+{
+  private double quantidade;
+
+  public ItemPedido(double quantidade)
+  {
+    this.quantidade = quantidade;
+  }
+}
+
+
--- setup-vim.sh
+++ setup-vim.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 echo "[vim] setting up vim editor." 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Create a soft link to user vimrc
+if [ -f ~/.vimrc ]; then 
+  mv ~/.vimrc ~/.vimrc.old 
+elif [ -L ~/.vimrc ]; then 
+  rm -f ~/.vimrc
+fi
 
 ln -s ~/Workspace/dotfiles/vimrc ~/.vimrc
 
+# Dowload Vundle manager plugin. 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install YouCompleteMe dependencies for Fedora 35. 
+sudo dnf install cmake gcc-c++ make python3-devel
+
+# Install C and Java completers. The Python is installed by default.
+python3 /home/gomesluiz/.vim/bundle/YouCompleteMe/install.py --clang-completer --java-completer
